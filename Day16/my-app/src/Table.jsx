3e99c056ea0f3624ba2b25f4769f5296e872ca6d
Rohitkumar6394 <mrpalrohitpal@gmail.com>
2025-08-18 15:45:07 +0530

    feat: Implement main application structure with user input form and table integration

--- Day16/my-app/src/Table.jsx
+++ Day16/my-app/src/Table.jsx
@@ -1,57 +1,62 @@
-import { useState ,useEffect, use} from "react";
-import Pagination from "./pagination";
-function Table({ users }) {
+import React, { useEffect, useState } from 'react'
+import Pagination from './Pagination'
 
-    const [p_data, setP_data] = useState([])
-
-
-    const send_data =(data)=>{
+function Table({users,setData,setEditId,setDel_id}) {
+    const[p_data,setP_data]=useState([])
+    const [user_data,setUser_data]=useState({})
+    const send_data=(data)=>{
         setP_data(data)
     }
+    console.log(users);
+   console.log(p_data);
+   useEffect(()=>{
+        setP_data({...p_data, ["users_data"]:0})
+   },[])
+  useEffect(()=>{
+    setData(user_data)
+    setEditId(user_data.id)
+  },[user_data])
 
+//    to edit user data
+   const edit=(user_id)=>{
+        setUser_data(users.find((user)=>user.id==user_id))
+        
+   }
 
-   
-    // const send_data = (data) => {
-    //     setP_data(data)
-    //     console.log(p_data);
-    // }
+   const delet =(id)=>{
+          setDel_id(id)
+   }
+  return (
+    <div>
+        {p_data["users_data"] && <table border={1}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                { p_data["users_data"].map((user)=>(
+                    
+                    
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.email}</td>
+                        <td>{user.name}</td>
+                        <td><button onClick={()=>edit(user.id)}>Edit</button></td>
+                        <td><button onClick={()=>delet(user.id)}>Delete</button></td>
+                    </tr>
 
-    const styles = {
-        "border": "solid 1px black"
-    }
-    
-
-    useEffect(()=>{
-        setP_data({...p_data,["user_data"]:0})
-},[])
-
-    return (
-        <div>
-            <>
-                {p_data["users_data"] && (
-                    <table style={styles}>
-                        <thead>
-                            <tr>
-                                <th>id</th>
-                                <th>Email</th>
-                                <th>Name</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {p_data["users_data"].map((user) => (
-                                <tr key={user.id}>
-                                    <td>{user.id}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.name}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-                <Pagination users={users} send_data={send_data} />
-            </>
-        </div>
-
-    );
+                ))}
+            </tbody>
+        </table> 
 }
+        <Pagination users={users} send_data={send_data}/>   
+    </div>
+
+  )
+}
+
 export default Table
